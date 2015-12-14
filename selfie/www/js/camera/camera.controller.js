2b2cad9971e76d29a544585c65f96e738bdb7b62
Jean Lestang <jelestang@airfrance.fr>
2015-12-14 15:26:36 +0100

    Creation of CameraController. Test 1 is RED

---
+++ selfie/www/js/camera/camera.controller.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+    
+    angular.module('Selfie.Camera.module').
+    	controller('CameraController', CameraCtrl);
+        
+    var injectParams = [];
+    CameraCtrl.$inject = injectParams;
+    function CameraCtrl() {
+        /* jshint validthis: true */			
+        var vm = this;
+
+        vm.takePicture = function() {
+        }
+    }
+    
+})();
