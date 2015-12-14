e824d255e2aaa305e7aedd44ead13ec629ed3abf
Jean Lestang <jelestang@airfrance.fr>
2015-12-14 17:04:31 +0100

    Add DemosSteps: how to run the demo (in French)

---
+++ DemoSteps/120-créer CameraControllerSpec.js
@@ -0,0 +1,12 @@
+- Ecriture du test de création du service CameraController
+
+- création du répertoire test\unit
+- création du fichier CameraControllerSpec.js
+
+describe('Unit: CameraController', function() {
+    // Load the module to test, add your module name in here!
+    beforeEach(module('Selfie'));
+    
+});
+
+
